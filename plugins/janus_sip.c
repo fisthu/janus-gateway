9349b13d11c6e43b29ea409c4875d51179e619fe
Oki Fisthu Sasongko <fisthu@Okis-MacBook-Pro.local>
2019-04-02 02:39:05 +0700

    revert ordering janus log

--- plugins/janus_sip.c
+++ plugins/janus_sip.c
@@ -2165,9 +2165,9 @@ static void *janus_sip_handler(void *data) {
 				/* Got the values, try registering now */
 				JANUS_LOG(LOG_VERB, "Registering user %s (auth=%s, secret %s) @ %s through %s (outbound proxy: %s)\n",
 					username_text,
-					authuser_text != NULL ? authuser_text : username_text,
 					secret_text,
 					username_uri.url->url_host,
+					authuser_text != NULL ? authuser_text : username_text,
 					proxy_text != NULL ? proxy_text : "(null)",
 					obproxy_text != NULL ? obproxy_text : "none");
 			}
