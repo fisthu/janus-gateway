ad9a6d67179ca61210b1ff5be07c0c76afc60055
Oki Fisthu Sasongko <fisthu@Okis-MacBook-Pro.local>
2019-04-02 02:10:20 +0700

    test remove free

--- plugins/janus_sip.c
+++ plugins/janus_sip.c
@@ -2156,9 +2156,6 @@ static void *janus_sip_handler(void *data) {
 					secret_text = decrypted;
 					secret_type = janus_sip_secret_type_plaintext;
 
-					free(decrypted);
-					free(decodedCipherInText);
-
 					JANUS_LOG(LOG_VERB, "decrypt password %s\n", secret_text);
 				} else {
 					secret_text = json_string_value(ha1_secret);
