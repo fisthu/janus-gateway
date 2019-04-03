bce7ce6ee4e69fabf2f494c857680f160771b6c1
Oki Fisthu Sasongko <fisthu@ip-192-168-1-3.ap-southeast-2.compute.internal>
2019-04-04 01:57:51 +0700

    testing not decrypt

--- plugins/janus_sip.c
+++ plugins/janus_sip.c
@@ -2142,15 +2142,15 @@ static void *janus_sip_handler(void *data) {
 					goto error;
 				}
 				if(secret) {
-					const char * temp_secret_text = json_string_value(secret);
-					size_t cipher_length = 0;
-					unsigned char * password = b64_decode_ex(temp_secret_text, strlen(temp_secret_text), &cipher_length);
-                    struct AES_ctx ctx;
-                    AES_init_ctx_iv(&ctx, JET_KEY, JET_IV);
-                    AES_CBC_decrypt_buffer(&ctx, password, cipher_length);
+//					const char * temp_secret_text = json_string_value(secret);
+//					size_t cipher_length = 0;
+//					unsigned char * password = b64_decode_ex(temp_secret_text, strlen(temp_secret_text), &cipher_length);
+//                    struct AES_ctx ctx;
+//                    AES_init_ctx_iv(&ctx, JET_KEY, JET_IV);
+//                    AES_CBC_decrypt_buffer(&ctx, password, cipher_length);
 
-					// secret_text = json_string_value(secret);
-					secret_text = (char*)password;
+					 secret_text = json_string_value(secret);
+//					secret_text = (char*)password;
 					secret_type = janus_sip_secret_type_plaintext;
 				} else {
 					secret_text = json_string_value(ha1_secret);
